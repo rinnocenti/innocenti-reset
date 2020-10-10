aa97969bb00d0b10730bb2efab3f47bf1423aca7
Renato innocenti <renato.innocenti@gmail.com>
2020-10-10 20:36:05 -0300

    v 1.0

--- scripts/longrest.js
+++ scripts/longrest.js
@@ -7,125 +7,124 @@
  * 4 = Quarto Individual
  * 5 = Quarto de Luxo
  */
+import LongRestDialog from "../../../systems/dnd5e/module/apps/long-rest.js";
 
-export let LongRest = async function (category = 0) {
-    if (canvas.tokens.controlled.length === 0)
-        return ui.notifications.error("select a token");
-    for (let token of canvas.tokens.controlled) {
-        const data = token.actor.data.data;
-        // promessa de um dialogo..
-        let dialog = false; ////////////////////////////////////////////////////////// <-----------------------
-        let newDay = false;
-        if (dialog) {
-            try {
-                newDay = await LongRestDialog(token.actor);
-            } catch (err) {
-                return;
-            }
-        }
-        // Recover hit points to full, and eliminate any existing temporary HP
-        const updateData = {
-            "data.attributes.hp.temp": 0,
-            "data.attributes.hp.tempmax": 0
-        }
-        if (category <= 1) {
-            let halfhp = Math.round(data.attributes.hp.max / 2);
-            let conmod = (data.abilities.con.value - 10) / 2;
-            updateData["data.attributes.hp.value"] = (data.attributes.hp.value < halfhp) ? halfhp : data.attributes.hp.value + ((data.abilities.con.value - 10) / 2);
-        } else {
-            updateData["data.attributes.hp.value"] = data.attributes.hp.max;
-        }
-        //const dhp = data.attributes.hp.max - data.attributes.hp.value;
-        // Recover character resources
-        for (let [k, r] of Object.entries(data.resources)) {
-            if (r.max && (r.sr || r.lr)) {
-                updateData[`data.resources.${k}.value`] = r.max;
-            }
-        }
-        // Recover spell slots
-        for (let [k, v] of Object.entries(data.spells)) {
-            if (!v.max && !v.override) continue;
-            updateData[`data.spells.${k}.value`] = v.override || v.max;
-        }
 
-        // Recover pact slots.
-        const pact = data.spells.pact;
-        updateData['data.spells.pact.value'] = pact.override || pact.max;
-        let recoverHD = 1;
-        // Determine the number of hit dice which may be recovered
-        if (category <= 1) {
-            recoverHD = data.attributes.hd;
-        } else if (category === 2 || category === 3) {
-            recoverHD = 1;
-        } else if (category === 4) {
-            recoverHD = Math.max(Math.floor(data.details.level / 2), 1);
-        } else {
-            recoverHD = data.details.level;
+export let LongRest = async function (category = 0, dialog = true, newDay = true) {
+    const actor = game.actors.entities.find(a => a.name === canvas.tokens.controlled[0].actor.name);
+    const data = actor.data.data;
+    if (dialog) {
+        try {
+            newDay = await LongRestDialog.longRestDialog({ actor: actor });
+        } catch (err) {
+            return;
         }
-        //recover Exhaustion
-        //ui.notifications.info(data.attributes.exhaustion);
-        updateData["data.attributes.exhaustion"] = (data.attributes.exhaustion > 0) ? data.attributes.exhaustion - 1 : 0;
+    }
 
-        let dhd = 0;
-        // Sort classes which can recover HD, assuming players prefer recovering larger HD first.
-        const updateItems = token.actor.items.filter(item => item.data.type === "class").sort((a, b) => {
-            let da = parseInt(a.data.data.hitDice.slice(1)) || 0;
-            let db = parseInt(b.data.data.hitDice.slice(1)) || 0;
-            return db - da;
-        }).reduce((updates, item) => {
-            const d = item.data.data;
-            if ((recoverHD > 0) && (d.hitDiceUsed > 0)) {
-                let delta = Math.min(d.hitDiceUsed || 0, recoverHD);
-                recoverHD -= delta;
-                dhd += delta;
-                updates.push({ _id: item.id, "data.hitDiceUsed": d.hitDiceUsed - delta });
-            }
-            return updates;
+    // Recover hit points to full, and eliminate any existing temporary HP
+    let dhp = data.attributes.hp.max - data.attributes.hp.value;
+    const updateData = {
+        "data.attributes.hp.temp": 0,
+        "data.attributes.hp.tempmax": 0
+    };
+    if (category <= 1) {
+        let halfhp = Math.round(data.attributes.hp.max / 2);
+        let conmod = (data.abilities.con.value - 10) / 2;
+        dhp = (data.attributes.hp.value < halfhp) ? halfhp : data.attributes.hp.value + conmod;
+        updateData["data.attributes.hp.value"] = dhp;
+        dhp -= data.attributes.hp.value;
+    } else {
+        updateData["data.attributes.hp.value"] = data.attributes.hp.max;
+    }
+    // Recover character resources
+    for (let [k, r] of Object.entries(data.resources)) {
+        if (r.max && (r.sr || r.lr)) {
+            updateData[`data.resources.${k}.value`] = r.max;
+        }
+    }
+    // Recover spell slots
+    for (let [k, v] of Object.entries(data.spells)) {
+        if (!v.max && !v.override) continue;
+        updateData[`data.spells.${k}.value`] = v.override || v.max;
+    }
+
+    // Recover pact slots.
+    const pact = data.spells.pact;
+    updateData['data.spells.pact.value'] = pact.override || pact.max;
+    
+    // Determine the number of hit dice which may be recovered
+    let recoverHD = 0;
+    if (category === 0 || category === 2) {
+        recoverHD = data.attributes.hd;
+    } else if (category === 1 || category === 3) {
+        recoverHD = 1;
+    } else if (category === 4) {
+        recoverHD = Math.max(Math.floor(data.details.level / 2), 1);
+    } else {
+        recoverHD = data.details.level;
+    }
+ 
+    let dhd = 0;
+    // Sort classes which can recover HD, assuming players prefer recovering larger HD first.
+    const updateItems = actor.items.filter(item => item.data.type === "class").sort((a, b) => {
+        let da = parseInt(a.data.data.hitDice.slice(1)) || 0;
+        let db = parseInt(b.data.data.hitDice.slice(1)) || 0;
+        return db - da;
+    }).reduce((updates, item) => {
+        const d = item.data.data;
+        if ((recoverHD > 0) && (d.hitDiceUsed > 0) && (category > 1)) {
+            let delta = Math.min(d.hitDiceUsed || 0, recoverHD);
+            recoverHD -= delta;
+            dhd += delta;
+            updates.push({ _id: item.id, "data.hitDiceUsed": d.hitDiceUsed - delta });
+        }
+        return updates;
         }, []);
 
-        // Iterate over owned items, restoring uses per day and recovering Hit Dice
-        const recovery = newDay ? ["sr", "lr", "day"] : ["sr", "lr"];
-        for (let item of token.actor.items) {
-            const d = item.data.data;
-            if (d.uses && recovery.includes(d.uses.per)) {
-                updateItems.push({ _id: item.id, "data.uses.value": d.uses.max });
-            }
-            else if (d.recharge && d.recharge.value) {
-                updateItems.push({ _id: item.id, "data.recharge.charged": true });
-            }
+    // Iterate over owned items, restoring uses per day and recovering Hit Dice
+    const recovery = newDay ? ["sr", "lr", "day"] : ["sr", "lr"];
+    for (let item of actor.items) {
+        const d = item.data.data;
+        if (d.uses && recovery.includes(d.uses.per)) {
+            updateItems.push({ _id: item.id, "data.uses.value": d.uses.max });
         }
-        // Perform the updates
-        await token.actor.update(updateData);
-        if (updateItems.length) await token.actor.updateEmbeddedEntity("OwnedItem", updateItems);
+        else if (d.recharge && d.recharge.value) {
+            updateItems.push({ _id: item.id, "data.recharge.charged": true });
+        }
     }
-    return;
-}
-async function LongRestDialog({ actor } = {}) {
-    return new Promise((resolve, reject) => {
-        const dlg = new this(actor, {
-            title: "Long Rest",
-            buttons: {
-                rest: {
-                    icon: '<i class="fas fa-bed"></i>',
-                    label: "Rest",
-                    callback: html => {
-                        let newDay = false;
-                        if (game.settings.get("dnd5e", "restVariant") === "normal")
-                            newDay = html.find('input[name="newDay"]')[0].checked;
-                        else if (game.settings.get("dnd5e", "restVariant") === "gritty")
-                            newDay = true;
-                        resolve(newDay);
-                    }
-                },
-                cancel: {
-                    icon: '<i class="fas fa-times"></i>',
-                    label: "Cancel",
-                    callback: reject
-                }
-            },
-            default: 'rest',
-            close: reject
+
+    // Perform the updates
+    await actor.update(updateData);
+    if (updateItems.length) await actor.updateEmbeddedEntity("OwnedItem", updateItems);
+
+    // Display a Chat Message summarizing the rest effects
+    let restFlavor;
+    switch (game.settings.get("dnd5e", "restVariant")) {
+        case 'normal': restFlavor = game.i18n.localize(newDay ? "DND5E.LongRestOvernight" : "DND5E.LongRestNormal"); break;
+        case 'gritty': restFlavor = game.i18n.localize("DND5E.LongRestGritty"); break;
+        case 'epic': restFlavor = game.i18n.localize("DND5E.LongRestEpic"); break;
+    }
+
+    // Determine the chat message to display
+    if (chat) {
+        let lrMessage = "DND5E.LongRestResultShort";
+        if ((dhp !== 0) && (dhd !== 0)) lrMessage = "DND5E.LongRestResult";
+        else if ((dhp !== 0) && (dhd === 0)) lrMessage = "DND5E.LongRestResultHitPoints";
+        else if ((dhp === 0) && (dhd !== 0)) lrMessage = "DND5E.LongRestResultHitDice";
+        ChatMessage.create({
+            user: game.user._id,
+            speaker: { actor: actor, alias: actor.name },
+            flavor: restFlavor,
+            content: game.i18n.format(lrMessage, { name: actor.name, health: dhp, dice: dhd })
         });
-        dlg.render(true);
-    });
+    }
+
+    // Return data summarizing the rest effects
+    return {
+        dhd: dhd,
+        dhp: dhp,
+        updateData: updateData,
+        updateItems: updateItems,
+        newDay: newDay
+    }
 }
